5219b936b8cc704da57c57868a2d3fc444e3f963
SaschaNaz <saschanaz@outlook.com>
2015-11-24 02:51:37 +0900

    mapper->parser

---
+++ uwp-type-mapper/fspromise.ts
@@ -0,0 +1,60 @@
+﻿import * as fs from "fs"
+
+export function readFiles(path: string) {
+    return new Promise<string[]>((resolve, reject) => {
+        fs.readdir(path, (err, files) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(files);
+            }
+        })
+    });
+}
+
+export function readFile(path: string) {
+    return new Promise<string>((resolve, reject) => {
+        fs.readFile(path, "utf8", (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+export function writeFile(path: string, content: string) {
+    return new Promise<void>((resolve, reject) => {
+        fs.writeFile(path, content, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+export function makeDirectory(path: string) {
+    return new Promise<void>((resolve, reject) => {
+        fs.mkdir(path, (err) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        })
+    })
+}
+export function exists(path: string) {
+    return new Promise<boolean>((resolve, reject) => {
+        fs.exists(path, (exists) => {
+            resolve(exists);
+        });
+    });
+}
