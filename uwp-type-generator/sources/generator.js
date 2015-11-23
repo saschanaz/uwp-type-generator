33251cc893b893a072601aa3d024c00b9ea72239
SaschaNaz <saschanaz@outlook.com>
2015-11-23 23:10:21 +0900

    rename subproject

--- uwp-type-generator/sources/generator.js
+++
@@ -1,9 +0,0 @@
-function generate() {
-    enumerate(Windows);
-    function enumerate(obj) {
-        for (var item in obj) {
-            console.log(item);
-            enumerate(obj[item]);
-        }
-    }
-}
